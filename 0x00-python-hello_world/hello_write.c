58ab7a0164d16392575171e776618882d1b19122
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-04 14:01:42 +0000

    Add task 11

--- 0x00-python-hello_world/hello_write.c
+++ 0x00-python-hello_world/hello_write.c
@@ -1,14 +1,11 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 
 
 void hello_write(void)
 {
-	char s[] = "and that piece of art is useful - Dora Korpar, 2015-10-19";
-	int len = strlen(s);
-	write(2, s, len);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19", 59);
 	printf("\n");
 }
