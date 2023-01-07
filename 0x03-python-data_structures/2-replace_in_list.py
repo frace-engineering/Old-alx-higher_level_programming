e2a994986deff8de24f386020616d774bdb3f645
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-07 10:29:04 +0000

    [Fix] Task two

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,5 +3,5 @@ def replace_in_list(my_list, idx, element):
     for x in range(len(my_list)):
         if ((idx < 0) or (idx > len(my_list))):
             return my_list
-        my_list[idx] == element
+        else my_listdx] == element
         return my_list
