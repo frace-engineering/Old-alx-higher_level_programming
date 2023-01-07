14a967bfee1608e02dfd690d7e8b9cbd76228e15
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-07 12:22:25 +0000

    Add task four

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list is not None:
-        new_list = my_list
-        for x in range(len(my_list)):
-            if ((idx < 0) or (idx > len(my_list) - 1)):
-                return new_list
-            else:
-                new_list[idx] = element
-                return new_list
+    new_list = my_list
+    for x in range(len(my_list)):
+        if ((idx < 0) or (idx > len(my_list) - 1)):
+            return new_list
+        else:
+            new_list[idx] = element
+            return new_list
 
