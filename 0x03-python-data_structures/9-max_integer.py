492dad11a80af14ae9e1e2da3038c61068b8aca4
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-07 22:14:20 +0000

    [Fix] task nine

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
+    if my_list == " ":
         return None
     for x in range(len(my_list)):
         if my_list[x] > my_list[x + 1]:
