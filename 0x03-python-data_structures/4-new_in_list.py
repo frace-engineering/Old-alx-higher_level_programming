6f69fbc742a771d07e1a190a6ec81146ce96dd3a
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-07 11:59:25 +0000

    Add task four

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,6 +5,6 @@ def new_in_list(my_list, idx, element):
             if ((idx < 0) or (idx > len(new_list) - 1)):
                 return my_list
             else:
-                ne_list = my_list
+                ne_list = my_list.copy()
                 new_list[idx] = element
                 return new_list
