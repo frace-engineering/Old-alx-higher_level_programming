6b74fb50e9548838c45fb5c92f8eba8ab8af27fc
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-07 16:08:09 +0000

    [Fix] task five

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
     for j in my_string:
         if my_string[j] == " ":
             count += 1
-    for x in range(len(my_string) - count):
-        if (my_string[x] == 'c' or my_string[x] == 'C'):
-            my_string = my_string[x:] + my_string[x + 1:]
-    return my_string
+        for x in range(len(my_string) - count):
+            if (my_string[x] == 'c' or my_string[x] == 'C'):
+                my_string = my_string[x:] + my_string[x + 1:]
+        return my_string
