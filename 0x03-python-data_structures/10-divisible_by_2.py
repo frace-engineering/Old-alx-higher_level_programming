207dafc188d47bc3d6f601fb90af4bd343521823
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-08 18:51:10 +0000

    [Fix] task ten

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     for x in range(len(my_list)):
+        if (my_list[x] % 2 == 0):
+            print("{} is divisble by 2".format(my_list[x]))
+        else:
+            print("{} is not divisble by 2".format(my_list[x]))
         new_list[x] = True if my_list[x] % 2 == 0 else False
     return New_list
 
