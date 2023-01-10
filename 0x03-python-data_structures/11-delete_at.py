5895f630c37e1c2e76d81422b9c4f2da20768658
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-10 04:13:10 +0000

    [fix task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    for x in range(len(my_list)):
-        for idx in x:
-            del my_list[x] 
-    return my_list
+    if ((idx < 0) or (idx > len(my_list))):
+        return (my_list)
+    else:
+        for x in range(len(my_list) - 1):
+            if x == idx:
+                del my_list[x]
+        return (my_list)
