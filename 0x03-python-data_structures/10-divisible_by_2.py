abf873a3fc83342d82a2f5bceb2cbd7294fde7b0
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-10 03:39:33 +0000

    [Fix] task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for x in range(len(my_list)):
-        new_list = [True if i % 2 == 0 else False for i in my_list]
-    return New_list
-
+    new_list = my_list.copy()
+    for x in range(len(new_list)):
+        if new_list[x] % 2 == 0:
+            new_list[x] = True
+        else:
+            new_list[x] = False
+    return new_list
