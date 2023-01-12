7f24f89e78fe9ccf12ebe0eade7b0da88e21f46a
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-12 00:22:18 +0000

    [Fix] bugs in code

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     this_sum = 0
-    for x in my_list:
-        if my_list.count(x) == 1:
-            this_sum += x
+    for x in set(my_list):
+        this_sum += x
     print(this_sum)
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_disctionary:
-        a_dictionary[key] = new_value
-    else:
-        a_dictionary.update({key:value})
+    a_dictionary[key] = value
     return a_dictionary
