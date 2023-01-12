c7aededf292a6ddca6ea9728108b0e2eb59cd180
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-12 13:05:58 +0000

    [Fix] Bugs in task one

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    if my_list != ""
-        for x in my_list:
-            new_list.append(x)
-            for i in range(len(new_list)):
-                if new_list[i] == search:
-                    new_list[i] = replace
-        print(new_list)
+    new_list = [replace if x == search else x for x in my_list]
+    return (new_list)
