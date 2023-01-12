776f3cc8869859d8b24dc72228b6838055263f8c
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-12 13:40:26 +0000

    [Fix] Bug in task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    this_sum = 0
-    while (i < len(my_list)):
-        for x in my_list:
-            if my_list.count(x) == 1:
-                this_sum += x
-        i++
-    return (this_sum)
+    result = 0
+    for value in set(my_list):
+        result += value
+    return (result)
