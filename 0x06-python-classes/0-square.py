92a785f1ac69b2aad99bf4417ff2905bf33292a8
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-24 21:30:18 +0000

    Add task zero to six and nine

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+Class Square: Defines a square
+
+"""
+
+class square:
+    """ Make an empty Square Class """
+    pass
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Square Class: Define a Square with private instance
+
+'''
+
+class square:
+    ''' class square that defines a square '''
+    def __init__(self, size=0):
+        ''' initalize variables '''
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError('size must be >= 0')
+        elf.__size - size
