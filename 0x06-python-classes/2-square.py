f6d8735cdf00c3242cbb3c648b1e5ac65fe73743
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-26 00:05:22 +0000

    [Fix] bug in task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,11 +6,11 @@ Square Class: Define a Square with private instance
 
 
 class square:
-    ''' class square that defines a square '''
+    ''' class square defines a square '''
     def __init__(self, size=0):
         ''' initalize variables '''
+        self.size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if (size < 0):
             raise ValueError('size must be >= 0')
-        self.size = size
