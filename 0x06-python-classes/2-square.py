56632f40154180fe51710dd48ef2c93719b81427
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-01-26 00:12:02 +0000

    [Fix] bug in task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ class square:
     def __init__(self, size=0):
         ''' initalize variables '''
         self.__size = size
-        if type(size) is not int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
         if (size < 0):
             raise ValueError('size must be >= 0')
