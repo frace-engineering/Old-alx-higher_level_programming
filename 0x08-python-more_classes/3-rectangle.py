e94fe5705ac1b0e7933336c99bc0d0bd7ca42b93
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-02-01 21:29:13 +0000

    [Fix] bug in code

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-""" Class Rectangle """
-
-
 class Rectangle:
     """Defines a rectangle
     """
@@ -32,3 +29,36 @@ class Rectangle:
         """Getter method
         """
         return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Rectangle area
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Rectangle perimeter
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Creates a new string object from the given object
+        """
+        s = ''
+        if self.__width > 0 and self.__height > 0:
+            for _ in range(self.__height):
+                for _ in range(self.__width):
+                    s += '#'
+                s += '\n'
+        return s[: -1]
