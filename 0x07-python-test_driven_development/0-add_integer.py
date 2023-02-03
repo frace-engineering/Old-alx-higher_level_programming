dc4cc2a3f47f152fa3192494c3199c646f103b6a
Engr Ugwu Friday B <fugwungene@gmail.com>
2023-02-03 13:17:09 +0000

    [Fix] bug

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,12 +3,12 @@
 
 def add_integer(a, b=98):
     """ Adds two integers """
+    if a is not int or float:
+        raise TypeError("amust be an integer")
+    if b is not int or float:
+        raise TypeError("amust be an integer")
     if type(a) is float:
         a = int(a)
-    elif type(a) is not int:
-        raise TypeError("a must be an integer")
     if type(b) is float:
         b = int(b)
-    elif type(b) is not int:
-        raise TypeError("b must be an integer")
     return a + b
